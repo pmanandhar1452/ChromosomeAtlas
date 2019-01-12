07c257ccf97d5bcc9f496481486689168140cb75
pmanandhar1452 <jsaaymi@gmail.com>
2019-01-12 03:15:38 -0500

    Passes single var or subsp, need to format for multiple var or subsp

--- python/ChromosomeAtlas/LatexGenerator.py
+++ python/ChromosomeAtlas/LatexGenerator.py
@@ -1,12 +1,17 @@
 import re
 
 class LatexGenerator:
-    def parse_scientific_name(self, in_str):
+    def format_scientific_name(self, in_str):
         in_str = in_str.replace('\n', ' ')
-        matchObj = re.match( r'\s*(\w+\s+\w+)\s+var\.\s+(\w+)(\s+.*)?', in_str, re.I)
+        matchObj = re.match( r'\s*(\w+\.?\s+\w+)\s+(var|ssp|subsp)\.\s+(\w+)(\s+.*)?', in_str, re.I)
         if matchObj:
-            out_str = '{\\em ' + matchObj.group(1) + '} var. {\\em ' \
-                    + matchObj.group(2) + '}' + matchObj.group(3)
+            num_groups = len(matchObj.groups())
+            if num_groups == 4:
+                final_string = matchObj.group(4)
+            else:
+                final_string = ''
+            out_str = '{\\em ' + matchObj.group(1) + '} ' + matchObj.group(2) \
+                + '. {\\em ' + matchObj.group(3) + '}' + final_string
         else:
             matchObj = re.match( r'\s*(\w+\s+\w+)(.*)', in_str, re.I)
             if matchObj:
--- python/test_ChromosomeAtlas/test_LatexGenerator.py
+++ python/test_ChromosomeAtlas/test_LatexGenerator.py
@@ -9,15 +9,19 @@ class TestLatexGenerator(unittest.TestCase):
        'Bupleurum candoleii Wall. Ex DC.': '{\\em Bupleurum candoleii} Wall. Ex DC.',
        'I. sikkimensis': '{\\em I. sikkimensis}',
        'Achyranthes aspera var. porphyristachya (Wall. ex Moq.) Hook. f.':
-                '{\\em Achyranthes aspera} var. {\\em porphyristachya} (Wall. ex Moq.) Hook. f.',
-        'Astragalus stipulatus var. phuchokiensis H. Ohashi':
-                '{\\em Astragalus stipulatus} var. {\\em phuchokiensis} H. Ohashi'
+            '{\\em Achyranthes aspera} var. {\\em porphyristachya} (Wall. ex Moq.) Hook. f.',
+       'Astragalus stipulatus var. phuchokiensis H. Ohashi':
+           '{\\em Astragalus stipulatus} var. {\\em phuchokiensis} H. Ohashi',
+       'D. podocarpum ssp. fallax (Schindl.) H. Ohashi':
+           '{\\em D. podocarpum} ssp. {\\em fallax} (Schindl.) H. Ohashi',
+       'D. podocarpum subsp. oxyphyllum var. mandschuricum Maxim.':
+           '{\\em D. podocarpum} subsp. {\\em oxyphyllum} var. {\\em mandschuricum} Maxim.'
     }
 
-    def test_parse_scientific_name (self):
+    def test_format_scientific_name (self):
         lgen = LatexGen.LatexGenerator()
         for name in self.SCIENTIFIC_NAME_TRANSFORM.keys():
-            transformed_name = lgen.parse_scientific_name(name) 
+            transformed_name = lgen.format_scientific_name(name) 
             self.assertEqual(self.SCIENTIFIC_NAME_TRANSFORM[name], transformed_name)
             
 if __name__ == "__main__":

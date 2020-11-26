062f0611e6f6ce2fe6c2d5b8c47486d2e53b564f
Prakash Manandhar <jsaaymi@gmail.com>
2020-11-26 11:36:55 -0500

    Reformatted some headings and fonts

--- python/ChromosomeAtlas/LatexGenerator.py
+++ python/ChromosomeAtlas/LatexGenerator.py
@@ -4,6 +4,12 @@ from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
 import string
+from enum import Enum
+
+class CapStyle(Enum):
+    AS_IS     = 0
+    LOWER     = 1
+    CAP_WORDS = 2
 
 class LatexGenerator:
 
@@ -18,7 +24,7 @@ class LatexGenerator:
     # The ID and range of spreadsheets.
     SPREADSHEET_DICT = {
         'Acanthaceae': '1vbkdYUAIzmHhqRZCPx_wvImylWM_BqxcuhY2aIKZfuw', 
-        # 'Aceraceae'  : '1RqlxSLZs8Uhz2xyCFRq-541jyIqZRUejcGVX_WIPblw',
+        'Aceraceae'  : '1RqlxSLZs8Uhz2xyCFRq-541jyIqZRUejcGVX_WIPblw',
         # 'Agavaceae'  : '1IadRjWtV_dEsMEgmNhAFY2-viQ0Ug7fUmSyuh4km2Uk',
         # 'Aizoiceae'  : '1j_GgOSlBSEuzgbUOFU5p9eS8iixgbG-iASnL7u0g718',  
         # 'Alangiaceae': '1Z9bLIm1q21cLek5cq6_dv9eAmCjrlLhekkWxxDPLmhE',
@@ -32,12 +38,15 @@ class LatexGenerator:
     DONE_CODE_INDEX = 0
     SPECIES_INDEX = 1
     NEPAL_NAMES_INDEX = 2
+    NEPAL_NAMES_BIB_INDEX = 3
     ENGLISH_NAMES_INDEX = 4
-    USE_CODE_INDEX = 7
-    CHR_NUM_INDEX = 9
-    CITATION_INDEX = 10
-    NEPAL_DIST_INDEX = 11
-
+    ENGLISH_NAMES_BIB_INDEX = 5
+    USE_CODE_INDEX = 6
+    USE_CODE_BIB_INDEX = 7
+    CHR_NUM_INDEX = 8
+    CITATION_INDEX = 9
+    NEPAL_DIST_INDEX = 10
+    NEPAL_DIST_BIB_INDEX = 11
     
     NEPAL_SEARCH_WORDS = ['Godawari', 'Patan', 'Kuleswor']
 
@@ -63,6 +72,11 @@ class LatexGenerator:
     def output_citations(self, cite_string):
         cite_string = ''.join(cite_string.split())
         out_str = '\\citet{' + cite_string + '}\n\n'
+        self.output_latex_string_noreplace(out_str)
+
+    def output_citations_p(self, cite_string):
+        cite_string = ''.join(cite_string.split())
+        out_str = '\\citep{' + cite_string + '}\n'
         self.output_latex_string_noreplace(out_str)
 
     def format_scientific_name_list(self, in_str):
@@ -130,34 +144,67 @@ class LatexGenerator:
         # output chromosome numbers and citation as a separate row
         chr_num = row[self.CHR_NUM_INDEX].strip(',. ')
         citations = row[self.CITATION_INDEX].strip(',. ')
-        self.output_latex_string('\\noindent \\textbf{$' + chr_num + '$}: ')
-        self.output_citations(citations + '\n\n')
+        self.output_latex_string('\\begin{hangparas}{0.5cm}{1}\\noindent \\textbf{$' + chr_num + '$} ')
+        self.output_citations_p(citations)
+        self.output_latex_string('\\end{hangparas}\n')
 
+    def generate_latex_subheading(
+            self, row, str_heading, content, bib_index, capstyle):
+        if (capstyle == CapStyle.CAP_WORDS):
+            content = string.capwords(content)
+        elif (capstyle == CapStyle.LOWER):
+            content = content.lower()
+        self.output_latex_string(
+                    '\\begin{hangparas}{0.5cm}{1}\n'
+                    f'\\noindent \\textbf{{\small {str_heading}}}: ' \
+                    + '\\begin{otherlanguage}{hindi} ' \
+                    + '{' \
+                    + content + '} \\end{otherlanguage}\n')
+        bib = row[bib_index].strip(',. ')
+        self.output_latex_string('{\\small ')
+        self.output_citations_p(bib + '\n\n')
+        self.output_latex_string('}\\end{hangparas}\n\n\\vspace{2mm}')
+    
     def generate_latex_row(self, row):
-        print(f'generate_latex_row({row})')
+        # print(f'generate_latex_row({row})')
         if self.is_heading_row(row):
+            self.output_latex_string('\\vspace{5mm}')
             self.output_latex_string(
                 '\\section{' + self.format_scientific_name(row[self.SPECIES_INDEX]) + '}\n')
             if row[self.NEPAL_NAMES_INDEX] != '':
-                self.output_latex_string(
-                    '\\noindent \\textbf{Common name(s) in use in Nepal}: \\begin{hindi} ' \
-                    + '{\large ' \
-                    + (row[self.NEPAL_NAMES_INDEX]).lower() + '} \\end{hindi}\n\n')
+                self.generate_latex_subheading(
+                    row, 
+                    "Common Name(s) in use in Nepal",
+                    row[self.NEPAL_NAMES_INDEX], self.NEPAL_NAMES_BIB_INDEX,
+                    CapStyle.LOWER)
+                
             if row[self.ENGLISH_NAMES_INDEX] != '':
-                self.output_latex_string(
-                    '\\noindent \\textbf{Common name(s) in English}: ' \
-                    + string.capwords(row[self.ENGLISH_NAMES_INDEX], " ") + '\n\n')
+                self.generate_latex_subheading(
+                    row, 
+                    "Common Name(s) in English",
+                    row[self.ENGLISH_NAMES_INDEX], self.ENGLISH_NAMES_BIB_INDEX,
+                    CapStyle.CAP_WORDS)
+
             if row[self.USE_CODE_INDEX] != '':
-                self.output_latex_string(
-                    '\\noindent \\textbf{Uses}: ' + row[self.USE_CODE_INDEX] + '\n\n')
+                self.generate_latex_subheading(
+                    row, 
+                    "Uses",
+                    row[self.USE_CODE_INDEX], self.USE_CODE_BIB_INDEX,
+                    CapStyle.AS_IS)
+            
             if row[self.NEPAL_DIST_INDEX] != '':
-                self.output_latex_string(
-                    '\\noindent \\textbf{Distribution in Nepal/Local reports}: ' \
-                    + self.add_msl(row[self.NEPAL_DIST_INDEX]) + '\n\n')
+                self.generate_latex_subheading(
+                    row, 
+                    "Distribution in Nepal",
+                    self.add_msl(row[self.NEPAL_DIST_INDEX]), 
+                    self.NEPAL_DIST_BIB_INDEX,
+                    CapStyle.AS_IS)
+            
+            self.output_latex_string('\\vspace{4mm}\n\n') 
             
         # output chromosome numbers and citation as a separate row
         self.generate_latex_row_chr_num(row)
-
+        
     def add_msl(self, in_str):
        return re.sub(r'([0-9]+)', r'\1 msl', in_str)
 
@@ -216,19 +263,24 @@ class LatexGenerator:
             family_tex_file.write('\\chapter{' + family + '}\n\n' +
                 '\\input{' + family + '.count.tex}\n\n' +   
                 '\\input{' + family + '.table.tex}\n\n' + 
+                '\\vspace{5mm}'
                                 '\\bibliographystyle{plainnat}\n' + 
                                 '\\bibliography{Bibliography}\n')
             family_tex_file.close()
             family_count_file = open('output/' + family + '.count.tex', 'w')
+    
             if self.num_genus_in_fam == 1:
+                genus_particle = 'is a'
                 genus_word = 'genus'
+                
             else:
+                genus_particle = f'are {self.num_genus_in_fam}'
                 genus_word = 'genera'
 
             family_count_file.write(
-                '\n\nThere are %d %s and %d species reported with chromosome counts  '
-                'in Nepal in this family.\n\n'%
-                    (self.num_genus_in_fam, genus_word, self.num_species_in_fam)
+                'There %s %s and %d species reported with chromosome counts  '
+                'in Nepal in this family.\\vspace{-5mm}'%
+                    (genus_particle, genus_word, self.num_species_in_fam)
             )
             family_count_file.close()
         all_families_file.close()

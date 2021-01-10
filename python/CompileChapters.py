07823629b81d7432f0dad0d5b3bb6f7ef58247ee
Prakash Manandhar <jsaaymi@gmail.com>
2021-01-10 01:19:34 -0500

    Moved to new computer and streamlined compilation

--- python/CompileChapters.py
+++ python/CompileChapters.py
@@ -1,7 +1,8 @@
 import subprocess, sys
 import glob
-
+import os
 if __name__ == '__main__':
+    os.chdir("output")
     subprocess.call('xelatex main')
     aux_files = glob.glob("./*.aux")
     for af in aux_files:
--- python/RunCopySequence.bat
+++ python/RunCopySequence.bat
@@ -1,3 +1,8 @@
- robocopy "C:\Users\Prakash\Dropbox (MIT)\Apps\Overleaf\ChromosomeNumberReportsBook_Laxmi_2020" ./output /MIR /XX /XO
- python -m ChromosomeAtlas.LatexGenerator
- robocopy .\output "C:\Users\Prakash\Dropbox (MIT)\Apps\Overleaf\ChromosomeNumberReportsBook_Laxmi_2020" /MIR /XX /XO
+if not exist "output" mkdir output
+copy "C:\Users\laxmi\Google Drive\Laxmi Manandhar Book Publication\references\final-merged-bibtex\Bibliography.bib" .\output\
+robocopy "C:\Users\laxmi\Google Drive\Laxmi Manandhar Book Publication\LatexSrc\ChromosomeNumberReportsBook_Laxmi_2020" .\output\ /E
+python -m ChromosomeAtlas.LatexGenerator
+python CompileChapters.py
+For /f "tokens=1,2,3,4,5 delims=/. " %%a in ('date/T') do set CDate=%%d-%%b-%%c
+For /f "tokens=1,2 delims=:" %%f in ('time /t') do set CTime=%%f%%g
+copy .\output\main.pdf "C:\Users\laxmi\Google Drive\Laxmi Manandhar Book Publication\output_pdfs\%CDate%-%CTime%.pdf"

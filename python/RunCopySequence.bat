ccd0ba47b5d2dcc068b56d5972585265cd210f9f
Prakash Manandhar <jsaaymi@gmail.com>
2022-07-04 18:05:24 -0400

    Change google drive root

--- python/RunCopySequence.bat
+++ python/RunCopySequence.bat
@@ -1,8 +1,8 @@
 if not exist "output" mkdir output
-copy "C:\Users\laxmi\Google Drive\Laxmi Manandhar Book Publication\references\final-merged-bibtex\Bibliography.bib" .\output\
-robocopy "C:\Users\laxmi\Google Drive\Laxmi Manandhar Book Publication\LatexSrc\ChromosomeNumberReportsBook_Laxmi_2021" .\output\ /E
+copy "G:\Laxmi Manandhar Book Publication\references\final-merged-bibtex\Bibliography.bib" .\output\
+robocopy "G:\Laxmi Manandhar Book Publication\LatexSrc\ChromosomeNumberReportsBook_Laxmi_2021" .\output\ /E
 python -m ChromosomeAtlas.LatexGenerator
 python CompileChapters.py
 For /f "tokens=1,2,3,4,5 delims=/. " %%a in ('date/T') do set CDate=%%d-%%b-%%c
 For /f "tokens=1,2 delims=:" %%f in ('time /t') do set CTime=%%f%%g
-copy .\output\main.pdf "C:\Users\laxmi\Google Drive\Laxmi Manandhar Book Publication\output_pdfs\%CDate%-%CTime%.pdf"
+copy .\output\main.pdf "G:\Laxmi Manandhar Book Publication\output_pdfs\%CDate%-%CTime%.pdf"

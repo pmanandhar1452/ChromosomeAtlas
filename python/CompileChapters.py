a8858c37210a4afcc25700106dbf834985f73cc9
Prakash Manandhar <jsaaymi@gmail.com>
2022-07-09 21:19:36 -0400

    Refacor process_part

--- python/CompileChapters.py
+++ python/CompileChapters.py
@@ -1,37 +1,21 @@
 import subprocess, sys
 import glob
 import os
+
+def process_part(partName):
+    p = subprocess.Popen(f'xelatex {partName}', shell=True)
+    p.wait()
+    aux_files = glob.glob("./*.aux")
+    for af in aux_files:
+        p = subprocess.Popen(f'bibtex {af}', shell=True)
+        p.wait()
+    p = subprocess.Popen(f'xelatex {partName}', shell=True)
+    p.wait()
+    p = subprocess.Popen(f'xelatex {partName}', shell=True)
+    p.wait()
+
 if __name__ == '__main__':
     os.chdir("output")
-    p = subprocess.Popen('xelatex mainA', shell=True)
-    p.wait()
-    aux_files = glob.glob("./*.aux")
-    for af in aux_files:
-        p = subprocess.Popen(f'bibtex {af}', shell=True)
-        p.wait()
-    p = subprocess.Popen('xelatex mainA', shell=True)
-    p.wait()
-    p = subprocess.Popen('xelatex mainA', shell=True)
-    p.wait()
-
-    p = subprocess.Popen('xelatex mainB', shell=True)
-    p.wait()
-    aux_files = glob.glob("./*.aux")
-    for af in aux_files:
-        p = subprocess.Popen(f'bibtex {af}', shell=True)
-        p.wait()
-    p = subprocess.Popen('xelatex mainB', shell=True)
-    p.wait()
-    p = subprocess.Popen('xelatex mainB', shell=True)
-    p.wait()
-
-    p = subprocess.Popen('xelatex mainC', shell=True)
-    p.wait()
-    aux_files = glob.glob("./*.aux")
-    for af in aux_files:
-        p = subprocess.Popen(f'bibtex {af}', shell=True)
-        p.wait()
-    p = subprocess.Popen('xelatex mainC', shell=True)
-    p.wait()
-    p = subprocess.Popen('xelatex mainC', shell=True)
-    p.wait()
+    process_part("mainA")
+    process_part("mainB")
+    process_part("mainC")

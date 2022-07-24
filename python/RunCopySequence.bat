091ae159ffe23f85d6f4fede6407665ab0730443
Prakash Manandhar <jsaaymi@gmail.com>
2022-07-24 02:08:55 -0400

    Add UseAcryonyms table

--- python/RunCopySequence.bat
+++ python/RunCopySequence.bat
@@ -1,7 +1,7 @@
 if not exist "output" mkdir output
 copy "G:\.shortcut-targets-by-id\1L9Iud7qS4SmO_696Y15FRTHgKeIuPCJ9\Laxmi Manandhar Book Publication\references\final-merged-bibtex\Bibliography.bib" .\output\
 robocopy "G:\.shortcut-targets-by-id\1L9Iud7qS4SmO_696Y15FRTHgKeIuPCJ9\Laxmi Manandhar Book Publication\LatexSrc\ChromosomeNumberReportsBook_Laxmi_2021" .\output\ /E
-python -m ChromosomeAtlas.LatexGenerator
+::python -m ChromosomeAtlas.LatexGenerator
 python CompileChapters.py
 For /f "tokens=1,2,3,4,5 delims=/. " %%a in ('date/T') do set CDate=%%d-%%b-%%c
 For /f "tokens=1,2 delims=:" %%f in ('time /t') do set CTime=%%f%%g

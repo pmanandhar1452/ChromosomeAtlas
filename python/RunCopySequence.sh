6206a23b3e7d587796a237a1e809e8e5875a91ed
Prakash Manandhar <jsaaymi@gmail.com>
2023-05-11 11:53:35 -0400

    Preparing for run of Part 2

--- python/RunCopySequence.sh
+++ python/RunCopySequence.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-FOLDER=~/Google\ Drive/Laxmi\ Manandhar\ Book\ Publication
+FOLDER=~/Library/CloudStorage/GoogleDrive-engineer.manandhar@gmail.com/.shortcut-targets-by-id/1L9Iud7qS4SmO_696Y15FRTHgKeIuPCJ9/Laxmi\ Manandhar\ Book\ Publication
 mkdir -p output
 cp "$FOLDER"/references/final-merged-bibtex/Bibliography.bib ./output/
 cp -r "$FOLDER"/LatexSrc/ChromosomeNumberReportsBook_Laxmi_2021/* ./output/
@@ -9,4 +9,6 @@ python3.9 CompileChapters.py
 #For /f "tokens=1,2,3,4,5 delims=/. " %%a in ('date/T') do set CDate=%%d-%%b-%%c
 #For /f "tokens=1,2 delims=:" %%f in ('time /t') do set CTime=%%f%%g
 NOW=`date '+%F-%l%M %p'`
-cp ./output/main.pdf "$FOLDER"/output_pdfs/"$NOW".pdf
+cp ./output/mainA.pdf "$FOLDER"/output_pdfs/"$NOW-A".pdf
+cp ./output/mainB.pdf "$FOLDER"/output_pdfs/"$NOW-B".pdf
+cp ./output/mainC.pdf "$FOLDER"/output_pdfs/"$NOW-C".pdf

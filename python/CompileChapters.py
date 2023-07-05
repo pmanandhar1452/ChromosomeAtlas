41d6e42d3264c63dd73d04b0e4f6938582ae466b
Prakash Manandhar <jsaaymi@gmail.com>
2023-07-05 14:10:51 -0700

    Update before finalizing Part II

--- python/CompileChapters.py
+++ python/CompileChapters.py
@@ -2,31 +2,34 @@ import subprocess, sys
 import glob
 import os
 
+
 def process_part_frontmatter(partName):
-    p = subprocess.Popen(f'xelatex {partName}', shell=True)
+    p = subprocess.Popen(f"xelatex {partName}", shell=True)
     p.wait()
-    p = subprocess.Popen(f'bibtex Introduction.aux', shell=True)
+    p = subprocess.Popen(f"bibtex Introduction.aux", shell=True)
     p.wait()
-    p = subprocess.Popen(f'bibtex Preface.aux', shell=True)
+    p = subprocess.Popen(f"bibtex Preface.aux", shell=True)
     p.wait()
-    p = subprocess.Popen(f'xelatex {partName}', shell=True)
+    p = subprocess.Popen(f"xelatex {partName}", shell=True)
     p.wait()
-    p = subprocess.Popen(f'xelatex {partName}', shell=True)
+    p = subprocess.Popen(f"xelatex {partName}", shell=True)
     p.wait()
 
+
 def process_part(partName):
-    p = subprocess.Popen(f'xelatex {partName}', shell=True)
+    p = subprocess.Popen(f"xelatex {partName}", shell=True)
     p.wait()
     aux_files = glob.glob("./*.aux")
     for af in aux_files:
-        p = subprocess.Popen(f'bibtex {af}', shell=True)
+        p = subprocess.Popen(f"bibtex {af}", shell=True)
         p.wait()
-    p = subprocess.Popen(f'xelatex {partName}', shell=True)
+    p = subprocess.Popen(f"xelatex {partName}", shell=True)
     p.wait()
-    p = subprocess.Popen(f'xelatex {partName}', shell=True)
+    p = subprocess.Popen(f"xelatex {partName}", shell=True)
     p.wait()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     os.chdir("output")
     process_part_frontmatter("mainA")
     process_part("mainA")
